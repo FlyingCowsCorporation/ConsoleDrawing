2d53315d635e6ee0db4a480407f8258f1537715d
Umit Tokmak <44272098+umittokmak42@users.noreply.github.com>
2019-04-13 12:02:00 +0200

    TriangleAlt2

---
+++ src/TriangleAlt2.java
@@ -0,0 +1,26 @@
+
+public class TriangleAlt2 extends Shape {
+    public TriangleAlt2(int SIZEX, int SIZEY) {
+        super(SIZEX, SIZEY);
+        fill();
+    }
+
+    @Override
+    public void fill() {
+        // TODO
+        int half = SIZEX / 2;
+        int offSet = 0;
+
+
+        for (int y = SIZEY - 1; y >= 0; y--) {
+
+            for (int x = half - offSet; x < half + 1 + offSet; x++) {
+
+                fillPixel(x, y);
+            }
+
+            offSet++;
+        }
+    }
+}
+
