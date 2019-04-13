a8208e2ab014eb49eb813bed58af82b8ae28746d
prepless <42713054+prepless@users.noreply.github.com>
2019-04-13 11:51:20 +0200

    Driehoek toegevoegd, werkt nog niet optimaal

---
+++ src/TriangleAlt.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+
+public class TriangleAlt extends Shape {
+    public TriangleAlt(int SIZEX, int SIZEY) {
+        super(SIZEX, SIZEY);
+        fill();
+    }
+
+    @Override
+    public void fill() {
+
+        double B = (double) SIZEY;
+        double D = (double) SIZEX / 2;
+        double factor = 0;
+
+        ArrayList<Integer> extraStarsArray = new ArrayList<>();
+        //fill left side
+        for (int a = 0; a < SIZEX; a++) {
+            if (a < D) {
+                factor = a * B / D;//calculate the factor of current pixel
+                int extraStars = 0;
+                for (int b = 0; b < SIZEY; b++) {
+                    if (b < factor) { //if pos of pixel = lower then the factor
+                        extraStars++;
+                        fillPixel(a, b);
+                    }
+                }
+                extraStarsArray.add(extraStars);
+            }
+            //fill middle
+            if (a == D) {
+                for(int y = 0; y<SIZEY;y++) {
+                    fillPixel((int) D, y);
+                }
+            }
+        }
+        //fill right side with a copy of left side
+        int counter = 0;
+        for (int x = SIZEX; x > SIZEX/2;x--){
+            for(int y =0; y<extraStarsArray.get(counter);y++){
+                fillPixel(x, y);
+            }
+            counter++;
+        }
+    }
+}
